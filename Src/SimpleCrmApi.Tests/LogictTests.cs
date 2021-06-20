4bb167b21d1dd5b695d3398df8f203215f186a7b
DESKTOP-U2PG6KA\ivano <ivanov_nikolay@bk.ru>
2021-06-20 18:32:49 +0300

    add test project

---
+++ Src/SimpleCrmApi.Tests/LogictTests.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace SimpleCrmApi.Tests
+{
+    public class LogictTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}

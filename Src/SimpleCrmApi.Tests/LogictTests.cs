d683cde21780681bc51dcd3bcf6e4d9942a88742
Nik2065 <ivanov_nikolay@bk.ru>
2022-02-09 23:11:25 +0300

    add first inmemory db tests

--- Src/SimpleCrmApi.Tests/LogictTests.cs
+++ Src/SimpleCrmApi.Tests/LogictTests.cs
@@ -1,13 +1,66 @@
 using NUnit.Framework;
+using Microsoft.EntityFrameworkCore.InMemory;
+using Microsoft.EntityFrameworkCore;
+using DataAccess;
+using DataAccess.Entities;
+using Logic;
 
 namespace SimpleCrmApi.Tests
 {
     public class LogictTests
     {
+        public LogictTests()
+        {
+            var dbContextOptions = new DbContextOptionsBuilder<DataStore>()
+            .UseInMemoryDatabase("InMemoryDatabase")
+            .Options;
+
+            _db = new DataStore(dbContextOptions);
+
+            //prepearing data
+
+            // Roles
+            //
+            var r1 = new CrmUserRole { CrmUserRoleId = 1, Code = "Operator" };
+            var r2 = new CrmUserRole { CrmUserRoleId = 2, Code = "Manager" };
+            var r3 = new CrmUserRole { CrmUserRoleId = 3, Code = "Administrator" };
+            _db.CrmUsersRoles.AddRange(r1, r2, r3);
+            _db.SaveChanges();
+
+            // TestUser
+            //
+
+
+        }
+
+
         [SetUp]
         public void Setup()
         {
 
+
+        }
+
+        private readonly DataStore _db;
+        //
+        private const string TestUserEmail = "mail@projectsrv.ru";
+
+
+
+
+        [Test]
+        public void CanCreateTestUser()
+        {
+            var userLogic = new UserLogic(_db);
+            userLogic.CreateTestUser();
+
+            //check that user in test database
+            var dbuser = _db.CrmUsers.FirstOrDefaultAsync(x => x.CrmUserEmail == TestUserEmail);
+
+            if (dbuser == null)
+                Assert.Fail("Cannot find test user");
+
+            Assert.Pass();
         }
 
         [Test]
